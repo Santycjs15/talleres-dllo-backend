d17d479b64d5cd5a6e80c0c60e05e478413a63f1
Santiago Cardona <sacardona@uninorte.edu.co>
2024-08-27 17:14:41 -0500

    Me falta la ultima

--- taller02.js
+++ taller02.js
@@ -1,23 +1,49 @@
 // Punto 1
-function findMax(entrada){
-    let valorMax = entrada[0];
-    for (let i = 0; i < entrada.length; i++) {
-        if(valorMax < entrada[i]){
-            valorMax = entrada[i];
+function findMax(lista){
+    let valorMax = lista[0];
+    for (let i = 0; i < lista.length; i++) {
+        if(valorMax < lista[i]){
+            valorMax = lista[i];
         }
     }
-    return valorMax
+    return valorMax;
 }
 
-console.log(findMax([3, 17, -1, 4, -19]));
+console.log(findMax([8, 17, 18, -30, -19]));
 
 //Punto 2
 function includes(lista, numero){
-
+let b = Boolean(false);
     for (let i = 0; i < lista.length; i++) {
         if(numero == lista[i]){
-            
-        }
+            b = Boolean(true);
+        }   
+    }
+    return b;
+}
+
+console.log(includes([3, 17, -1, 4, -19], 4));
+
+//Punto 3
+function sum(lista){
+    let sumaTotal = lista[0];
+    for (let i = 1; i < lista.length; i++) {
+        sumaTotal = lista[i] + sumaTotal;    
+    }
+    return sumaTotal;
+}
+
+console.log(sum([3, 17, -1, 4, -19]));
+
+//Punto 4
+
+function missingNumbers(lista){
+    let ultNum = lista[lista.length - 1];
+    for (let i = 1; i < ultNum; i++) {
+        const element = lista[i];
         
     }
+    return ultNum
 }
+
+console.log(missingNumbers([7, 2, 4, 6, 3, 9]));
