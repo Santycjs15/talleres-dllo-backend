39e38bfc0f4fb34669812a6ec49df16f3039c94a
Santiago Cardona <sacardona@uninorte.edu.co>
2024-08-27 16:26:46 -0500

    Primera pregunta

---
+++ taller02.js
@@ -0,0 +1,23 @@
+// Punto 1
+function findMax(entrada){
+    let valorMax = entrada[0];
+    for (let i = 0; i < entrada.length; i++) {
+        if(valorMax < entrada[i]){
+            valorMax = entrada[i];
+        }
+    }
+    return valorMax
+}
+
+console.log(findMax([3, 17, -1, 4, -19]));
+
+//Punto 2
+function includes(lista, numero){
+
+    for (let i = 0; i < lista.length; i++) {
+        if(numero == lista[i]){
+            
+        }
+        
+    }
+}
